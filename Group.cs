6fba4564f0273dd8f98543a4273f26cabbceedb6
Paul Bond <git@paulsbond.co.uk>
2018-12-30 10:58:15 +0800

    Added group Name property

--- Group.cs
+++ Group.cs
@@ -20,6 +20,32 @@ namespace Sudoku
             }
         }
 
+        public string Name
+        {
+            get
+            {
+                var firstRow = Cells[0].Row;
+                var firstCol = Cells[0].Col;
+                if (Cells.All(c => c.Row == firstRow)) return "Row " + (firstRow + 1);
+                if (Cells.All(c => c.Col == firstCol)) return "Col " + (firstCol + 1);
+                if (firstRow < 3)
+                {
+                    if (firstCol < 3) return "Top Left Box";
+                    if (firstCol < 6) return "Top Middle Box";
+                    return "Top Right Box";
+                }
+                if (firstRow < 6)
+                {
+                    if (firstCol < 3) return "Middle Left Box";
+                    if (firstCol < 6) return "Middle Box";
+                    return "Middle Right Box";
+                }
+                if (firstCol < 3) return "Bottom Left Box";
+                if (firstCol < 6) return "Bottom Middle Box";
+                return "Bottom Right Box";
+            }
+        }
+
         public void RemoveCandidate(int number)
         {
             Candidates.Remove(number);
